e7d5cae9bfadd82dfdad7f5e9b0dedf264ac9864
Curtis Schofield <curtis@ram9.cc>
2012-02-14 13:35:01 -0800

    all tests passing
    working in console as follows
    
    ///example : Given some html
    
    var fixture  = "<div class='phrase'><span id='first'
    class='text'>CAKE1</span></div>"
    fixture += "<div class='phrase'><span id='second'
    class='text'>CAKE2</span></div>"
    fixture += "<div class='phrase'><span id='third'
      class='text'>CAKE3</span></div>"
    
    // create part of dom
    $('body').append(fixture)
    
    // turn on Editable
    Editable('.phrase .text')
    
    // features
    // hover state
    // mouse click turns on edit
    // tab moves to next
    // and to first

--- app/assets/javascripts/editable_list.js
+++ app/assets/javascripts/editable_list.js
@@ -1,6 +1,106 @@
 Editable = function(selector) {
-  var self = this;
+  var self = this
+  self.selector = selector
+
+  var replace_tag_from = function(tag_name,node) {
+    var elem = $('<'+tag_name+'/>');
+    elem.attr('class',node.attr('class'))
+    elem.attr('id',node.attr('id'))
+    elem.attr('editable-index',node.attr('editable-index'))
+    elem.data(node.data())
+    elem.data('editable-was',node.get(0).tagName)
+    node.replaceWith(function() {
+      return elem
+    })
+    return elem
+  }
+
+  var replace_input_from = function(node) {
+    var element = replace_tag_from('input',node)
+    element.val(node.text())
+    return element
+  }
+
+  self.begin_edit_event = function(event) {
+      var clicked_node = $(this);
+      var new_node = replace_input_from(clicked_node);
+      install_edit_complete_notions(new_node)
+      new_node.focus();
+  }
+  var is_blur_tab_redirect = function () {
+    if (self.last_keydown_event) {
+      if (self.last_keydown_event.keyCode == Editable.TAB_KEY) {
+        return true
+      }
+    }
+    return false
+  }
+  var move_to_next_sibling = function(element) {
+    if(element.attr('editable-index')) {
+      var next_position = parseInt(element.attr('editable-index')) +1
+      var next_editable = $(selector).filter('[editable-index='+next_position+']')
+      if (next_editable.size() == 0) {
+        next_editable = $(selector).filter('[editable-index=1]')
+      }
+      next_editable.trigger('click.editable')
+    }else {
+      throw 'Expected to be able to assign custom attribute editable-index'
+    }
+  }
+  self.complete_edit_event = function(event) {
+
+    var finished_node = $(this)
+    var element = replace_tag_from(finished_node.data('editable-was'),finished_node)
+    element.text(finished_node.val())
+    install_edit_notions(element)
+    
+    // If tab key was hit during the edit phase
+    // we want to redirect this blur to be a click on the next
+    // sibling
+    if(is_blur_tab_redirect()) {
+      move_to_next_sibling(element)
+    }
+
+    // Cleanup - cross event state
+    // there was no last keydown event
+    self.last_keydown_event = undefined;
+  }
+  
+  var install_edit_notions = function(selector) {
+
+    $(selector).bind('click.editable',begin_edit_event)
+    $(selector).bind('mouseover.editable',function() {
+      $(this).addClass('editable-hover')
+    })
+
+    $(selector).bind('mouseout.editable',function() {
+      $(this).removeClass('editable-hover')
+    })
+  }
+
+  var install_edit_complete_notions = function (selector) {
+
+    $(selector).bind('keydown.editable',function(e) {
+      self.last_keydown_event = e
+      if(is_blur_tab_redirect()) {
+        $(this).trigger('blur.editable')
+        return false; // manually handle tab - no event propigation
+      }
+        return true; // allow default propigation
+    })
+
+    $(selector).bind('blur.editable',complete_edit_event)
+  }
+  var annotate_editable_with_position = function(selector) {
+    $(selector).each(function(i,el) {
+      $(el).attr('editable-index',i+1)
+    })
+  }
+  
+  install_edit_notions(selector)
+  annotate_editable_with_position(selector)
+
   $(selector).data('editable','installed')
   return self;
 }
-
+Editable.TAB_KEY = 9 // tab is #9
--- spec/javascripts/editable_list_spec.js
+++ spec/javascripts/editable_list_spec.js
@@ -1,28 +1,231 @@
 describe("Editable",function() {
-  beforeEach(function(){
-    fixture = "<div class='phrase'><span class='text'>CAKE</span></div>";
-    setFixtures(fixture);
-  });
+  var selector = '.phrase .text'
+
   it("it has a constructor", function() {
     expect(Editable).toBeDefined()
     expect(Editable()).toBeDefined()
   })
-  it("Editable list takes a selector to install itself with",function() {
-    Editable('.phrase .text')
-    expect($('.phrase .text').data('editable')).toBeDefined()
-  })
-  describe("completed edit event handler", function() {
-    it("transmits the input data from the current editable to the server",function() {
 
+  describe("with a single match in dom",function() {
+
+    beforeEach(function(){
+      var fixture = "<div class='phrase'><span class='text'>CAKE</span></div>"
+      setFixtures(fixture)
+    });
+
+    it("Editable list takes a selector to install itself with",function() {
+      Editable(selector)
+      expect($(selector).data('editable')).toBeDefined()
+    })
+
+    describe("with editable declared",function() {
+      beforeEach(function() {
+
+        Editable(selector)
+      })
+
+      describe("click event handler", function() {
+        it("turns a element into an input field",function() {
+          // best jquery practice - use pseudo selectors after
+          // using natural css selectors
+          expect($(selector).filter(':input').size()).toBe(0)
+          $(selector).filter(':first').trigger('click.editable')
+          expect($(selector).filter(':input').size()).toBe(1)
+        })
+        it("turns a element into an input field with the same classes",function() {
+          $(selector).addClass('cake').addClass('oat')
+          $(selector).filter(':first').trigger('click.editable')
+          expect($(selector).filter('.cake').size()).toBe(1)
+          expect($(selector).filter('.oat').size()).toBe(1)
+        })
+        it("turns a element into an input field with the same id",function() {
+          $(selector).attr('id','boom')
+          $(selector).filter(':first').trigger('click.editable')
+          expect($(selector).filter('#boom').size()).toBe(1)
+        })
+        it("turns a element CDATA text into input field value",function() {
+          $(selector).filter(':first').text("Lily")
+          $(selector).filter(':first').trigger('click.editable')
+          expect($(selector).filter(':first').val()).toEqual("Lily")
+        })
+        it("copies custom attr editable-index",function() {
+          $(selector).attr('editable-index','333')
+          $(selector).filter(':first').trigger('click.editable')
+          expect($(selector).filter(':input').attr('editable-index')).toBe('333')
+        })
+        it("turns a element into an input field with the previous data",function() {
+          // data is used in rails and jquery for storage of various properties
+          $(selector).data('hi',1)
+          $(selector).data('hello',2)
+          var expected_data = $(selector).data();
+          $(selector).filter(':first').trigger('click.editable')
+          // this is the general pattern for iterating correctly in a hash
+          // in javascript 
+          for(var prop in expected_data) {
+            // ignore parent prototype properties
+            if(expected_data.hasOwnProperty(prop)) {
+              // use proprety expected_data[prop]
+              expect($(selector).filter(':input').data(prop)).toEqual(expected_data[prop])
+            }
+          }
+          $(selector).filter(':input').data()
+        })
+        it("sets data element editable-was",function() {
+          var expected_tag =  $(selector).filter(':first').get(0).tagName
+          $(selector).filter(':first').trigger('click.editable')
+          expect($(selector).filter(':input').data('editable-was')).toEqual(expected_tag)
+        })
+        it("should focus the clicked element",function() {
+          expect($(selector).filter(':focus').size()).toBe(0)
+          $(selector).filter(':first').trigger('click.editable')
+          expect($(selector).filter(':focus').size()).toBe(1)
+
+        })
+      })
+
+      describe("completed edit event handler", function() {
+        it("returns element to editable-was",function() {
+          // first there is a mountain
+          var expected_tag =  $(selector).filter(':first').get(0).tagName
+          expect($(selector).filter(expected_tag).size()).toBe(1)
+          $(selector).filter(':first').trigger('click.editable')
+          // then there is no  mountain
+          expect($(selector).filter(expected_tag).size()).toBe(0)
+          $(selector).filter(':first').trigger('blur.editable')
+          // then there is
+          expect($(selector).filter(expected_tag).size()).toBe(1)
+        })
+        it("alters the CDATA text to the input value",function () {
+          $(selector).filter(':first').text("SOUP")
+          $(selector).filter(':first').trigger('click.editable')
+          expect($(selector).filter(':first').val()).toEqual("SOUP")
+          $(selector).filter(':first').val("are you a kitty?")
+          $(selector).filter(':first').trigger('blur.editable')
+          expect($(selector).filter(':first').text()).toEqual("are you a kitty?")
+
+        })
+        it("transmits the input data from the current editable to the server",function() {
+
+        })
+      })
+      describe("hover event handler", function() {
+        it("turns on editable hover class",function() {
+          expect($(selector).filter(':first').hasClass('editable-hover')).toBeFalsy()
+          $(selector).filter(':first').trigger('mouseover.editable')
+          expect($(selector).filter(':first').hasClass('editable-hover')).toBeTruthy()
+        })
+        it("turns off editable hover class",function() {
+          expect($(selector).filter(':first').hasClass('editable-hover')).toBeFalsy()
+          $(selector).filter(':first').trigger('mouseover.editable')
+          expect($(selector).filter(':first').hasClass('editable-hover')).toBeTruthy()
+          $(selector).filter(':first').trigger('mouseout.editable')
+          expect($(selector).filter(':first').hasClass('editable-hover')).toBeFalsy()
+        })
+      })
+
+      describe("editable life-cycle",function() {
+
+        it("moves from editable to to completed to editable to completed to editable",function() {
+          // invariant:
+          //  the selector always exists in the dom
+          //
+          // invariant:
+          //  the selector must become and input on click
+          //
+          expect($(selector).size()).toBe(1)
+          expect($(selector).filter(':input').size()).toBe(0)
+          $(selector).filter(':first').trigger('click.editable')
+          expect($(selector).size()).toBe(1)
+          expect($(selector).filter(':input').size()).toBe(1)
+          $(selector).filter(':first').trigger('blur.editable')
+          expect($(selector).size()).toBe(1)
+          expect($(selector).filter(':input').size()).toBe(0)
+          $(selector).filter(':first').trigger('click.editable')
+          expect($(selector).size()).toBe(1)
+          expect($(selector).filter(':input').size()).toBe(1)
+          $(selector).filter(':first').trigger('blur.editable')
+          expect($(selector).size()).toBe(1)
+          expect($(selector).filter(':input').size()).toBe(0)
+          $(selector).filter(':first').trigger('click.editable')
+          expect($(selector).size()).toBe(1)
+          expect($(selector).filter(':input').size()).toBe(1)
+        })
+      })
+
+
+      describe("enter key event handler", function() {
+        it("triggers complete edit event",function() {
+        })
+      })
+      describe("focus change event",function() {
+        it("triggers complete edit event",function() {
+        })
+      })
     })
   })
-  describe("click event handler", function() {
-    it("turns a element into an input field",function() {
 
-    })
-  })
-  describe("tab event handler", function() {
-    it("focuses a selected input field from current editable to the next known editable",function() {
+  describe("with multiple matches in dom",function() {
+    beforeEach(function(){
+      var fixture  = "<div class='phrase'><span id='first'  class='text'>CAKE1</span></div>"
+          fixture += "<div class='phrase'><span id='second' class='text'>CAKE2</span></div>"
+          fixture += "<div class='phrase'><span id='third'  class='text'>CAKE3</span></div>"
+      setFixtures(fixture)
+    });
+    describe("with editable declared",function() {
+      beforeEach(function() {
+        Editable(selector)
+      })
+      it("has a data editable-index",function() {
+        expect($(selector).filter('#first').attr("editable-index")).toEqual('1')
+        expect($(selector).filter('#second').attr("editable-index")).toEqual('2')
+        expect($(selector).filter('#third').attr("editable-index")).toEqual('3')
+
+      })
+      describe("editable life-cycle",function() {
+        describe("on editing node there is a keydown handler", function() {
+          describe("the tab key",function() {
+            it("completes edit and moves to next editable", function () {
+              // nothing if focused
+              expect($(selector).filter(':focus').size()).toBe(0)
+              $(selector).filter(':first').trigger('click.editable')
+              // #first becomes focused
+              expect($(selector).filter('#first').filter(':focus').size()).toBe(1)
+              var e = jQuery.Event("keydown.editable", { keyCode: Editable.TAB_KEY })
+              $(selector).filter(':first').trigger(e)
+
+              // #second becomes focused
+              expect($(selector).filter('#second').filter(':focus').size()).toBe(1)
+              // #second becomes input
+              expect($(selector).filter('#second').filter(':input').size()).toBe(1)
+            })
+            it("moves back to first editable after last", function () {
+              expect($(selector).filter(':focus').size()).toBe(0)
+
+              $(selector).filter('#third').trigger('click.editable')
+              expect($(selector).filter('#third').filter(':focus').size()).toBe(1)
+
+              var e = jQuery.Event("keydown.editable", { keyCode: Editable.TAB_KEY })
+              $(selector).filter('#third').trigger(e)
+
+              expect($(selector).filter('#first').filter(':focus').size()).toBe(1)
+              expect($(selector).filter('#first').filter(':input').size()).toBe(1)
+            })
+          })
+            it("other keys do nothing", function () {
+              // nothing if focused
+              expect($(selector).filter(':focus').size()).toBe(0)
+              $(selector).filter(':first').trigger('click.editable')
+              // #first becomes focused
+              expect($(selector).filter('#first').filter(':focus').size()).toBe(1)
+              var e = jQuery.Event("keydown.editable", { keyCode: 99 })
+              $(selector).filter(':first').trigger(e)
+              // nothing changes - still only one
+              expect($(selector).filter(':input').size()).toBe(1)
+              // and it is the same one
+              expect($(selector).filter('#first').filter(':focus').size()).toBe(1)
+            })
+        })
+      })
     })
   })
 })
